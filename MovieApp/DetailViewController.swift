e964aa6bef3a8b456eef0c20a028a928ee6d3f94
yewonbahn <banyaewon98@gmail.com>
2021-08-09 23:33:21 +0900

    play and stop plus

--- MovieApp/DetailViewController.swift
+++ MovieApp/DetailViewController.swift
@@ -22,6 +22,7 @@ class DetailViewController: UIViewController {
             descriptionLabel.font = .systemFont(ofSize: 16, weight: .light)
         }
     }
+    var player: AVPlayer?
     @IBOutlet weak var movieContainer: UIView!
     @IBOutlet weak var titleLabel: UILabel!{
         didSet{
@@ -30,29 +31,40 @@ class DetailViewController: UIViewController {
                                   weight: .medium)
         }
     }
+    //메모리에 올리긴했는데 여기서는 실제디바이스 크기가 어떤지는 확인하지 못함
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         titleLabel.text = movieResult?.trackName
         descriptionLabel.text = movieResult?.longDescription
-        if let hasURL = movieResult?.previewUrl
-        {
-            makePlayerAndPlay(urlString: hasURL)
+       
 
 }
         // Do any additional setup after loading the view.
-    }
     
-
+    override func viewDidAppear(_ animated: Bool) {
+        if let hasURL = movieResult?.previewUrl
+        {
+            makePlayerAndPlay(urlString: hasURL)
+    }
+    }
     func makePlayerAndPlay(urlString: String) {
         if let hasUrl = URL(string: urlString){
-            let player = AVPlayer(url: hasUrl)
+            self.player = AVPlayer(url: hasUrl)
             let playerLayer = AVPlayerLayer(player: player)
             
             movieContainer.layer.addSublayer(playerLayer)
             playerLayer.frame = movieContainer.bounds
-            player.play()
+            self.player?.play()
         }
   
+    }
+    @IBAction func play(_ sender: Any) {
+        
+        self.player?.play()
+    }
+    @IBAction func stop(_ sender: Any) {
+        self.player?.pause()
     }
     /*
     // MARK: - Navigation
